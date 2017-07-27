cf0b80466ebce6ca2eda7091b3b43f4895b5309c
inon19-meet <inon19@meet.mit.edu>
2017-07-27 14:10:14 +0300

    Stopping here

---
+++ inonnnnn.py
@@ -0,0 +1,13 @@
+def age('whats your age?'):
+    
+     for number in range(1,120):
+
+        print(number)
+
+        age = age + number
+
+        return
+
+print("this is your age in more many years XD ' ' :" + age)
+
+    
---
+++ inonnnnnnnnn1000.py
@@ -0,0 +1,5 @@
+c = 0
+for number in range(1,101):
+    print(number)
+    c = c + number
+print(c)
